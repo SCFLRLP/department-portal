7b22f91637929fa2a405c229c240370f65c45df4
sspccreates <145820126+sspccreates@users.noreply.github.com>
2024-03-16 18:33:08 -0400

    Update db.php

--- scflrlp-dep-portal/public_html/include/db.php
+++ scflrlp-dep-portal/public_html/include/db.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
-$host = "localhost"; // Change this to your database host
-$user = "id21983774_scflrlp"; // Change this to your database username
-$password = "agrernbETAte34!"; // Change this to your database password
-$database = "id21983774_scflrlp"; // Change this to your database name
+$host = "DATABASEHOST"; // Change this to your database host
+$user = "DATABASEUSERNAME"; // Change this to your database username
+$password = "DATABASEPASSWORD"; // Change this to your database password
+$database = "DATABASENAME"; // Change this to your database name
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
